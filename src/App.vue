4ec9fa41d8d59233a7a5f5e0e5092d39975ed5db
fadesyphe <pivkin.jura2017@gmail.com>
2023-12-06 00:58:54 +0600

    created BaseIcon component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <p></p>
+  <div></div>
 </template>
 
 <script>
---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg
+    :class="$attrs.class || 'w-7 h-7'"
+    :viewBox="$attrs.viewBox || '0 0 28 28'"
+    v-html="path"
+  ></svg>
+</template>
+
+<script>
+import icons from "../icons.js";
+export default {
+  data() {
+    return {
+      path: icons[this.name],
+    };
+  },
+
+  props: ["name"],
+};
+</script>
